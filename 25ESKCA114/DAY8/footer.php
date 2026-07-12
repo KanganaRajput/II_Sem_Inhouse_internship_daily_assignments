3a1339b4ddbf033130210930423bb0f924c0e93f
KanganaRajput <kangana1357@gmail.com>
2026-07-12 16:38:20 +0530

    Deleted DAY8 folder

--- 25ESKCA114/DAY8/footer.php
+++
@@ -1,8 +0,0 @@
-</div> <!-- container -->
-
-<footer class="bg-dark text-white text-center p-3 mt-5">
-    <p>© <?php echo date("Y"); ?> Student System | Made by Kangana</p>
-</footer>
-
-</body>
-</html>
